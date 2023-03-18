552d57b2068bf6c471a95398164725d89d4868f1
NEGROY <nerydiaz96@gmail.com>
2023-03-18 11:41:18 -0600

    primeras vistas

--- view/index.php
+++ view/index.php
@@ -19,41 +19,164 @@ include_once './ventana.php'
 <body class="dark">
   <?php require '../include/menu/menu1.php'; ?>
   <br>
-  <div class="container">
+  <div class="container" id="frame">
     <h1>ORDEN DE COMPRA</h1>
     <div class="card crdbody">
-      <div class="card-header text-info"> <h4>Documento</h4>  </div>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-sm-3">
-            <div class="form-control">
-              <input id="orden" type="number">
-              <label for="orden">Orden</label>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="form-control">
-              <input type="date" id="fecha">
-              <label for="fecha">Fecha</label>
-            </div>
-          </div>
-        </div>
+    <div class="card-header text-info"> <h4>Documento</h4>  </div>
+    <div class="card-body">
 
-        <div class="row crdbody">
+      <div class="row crdbody">
+        <div class="col-md-2"> <div class="input-group">
+          <span class="input-group-text">Orden</span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div> </div>
 
-<div class="col-md-5"> <div class="input-group">
-  <span class="input-group-text">Codigo de grupo</span>
-  <input type="text" class="form-control" id="CodigoGrupo" placeholder="Código de grupo" readonly>
-</div> </div>
+        <div class="col-md-3"> <div class="input-group">
+          <span class="input-group-text"> fecha </span>
+          <input type="date" class="form-control" id="NombreGrupo"  required>
+        </div> </div>
 
-<div class="col-md-7"> <div class="input-group">
-  <span class="input-group-text">Nombre del Grupo Solidario</span>
-  <input type="text" class="form-control" id="NombreGrupo" placeholder="Nombre Grupo" required>
-</div> </div>
-</div>
+        <div class="col-md-4"> <div class="input-group">
+          <span class="input-group-text"> tipo </span>
+          <input type="date" class="form-control" id="NombreGrupo"  required>
+        </div> </div>
+
       </div>
+
+      <div class="row crdbody">
+        <div class="col-md-3"> <div class="input-group">
+          <span class="input-group-text">Proveedor</span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div> </div>
+
+        <div class="col-md-4"> <div class="input-group">
+          <span class="input-group-text">  </span>
+          <input type="text" class="form-control" id="NombreGrupo" required>
+        </div> </div>
+
+        <div class="col-md-4"> <div class="input-group">
+          <span class="input-group-text"> NT  </span>
+          <input type="text" class="form-control" id="NombreGrupo" required>
+        </div> </div>
+
+      </div>
+
+    </div>
     </div> <!-- Ingreso de Caja FINAL -->
 
+    
+    <div class="card crdbody">
+    <div class="card-header text-info"> <h4>Variable de Control</h4>  </div>
+    <div class="card-body">
+      
+      <div class="row crdbody">
+        <div class="col-md-5"> <div class="input-group">
+          <span class="input-group-text">Autoriza</span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div> </div>
+
+        <div class="col-md-3"> <div class="input-group">
+          <span class="input-group-text"> Caja Chica </span>
+          <input type="checkbox" class="form-control" id="NombreGrupo"  required>
+        </div> </div>
+
+        <div class="col-md-4"> <div class="input-group">
+          <span class="input-group-text"> Combustible </span>
+          <input type="checkbox" class="form-control" id="NombreGrupo"  required>
+        </div> </div>
+
+      </div>
+
+      <div class="row crdbody">
+        <div class="col-md-3"> <div class="input-group">
+          <span class="input-group-text">Forma de Pago</span>
+          <input type="checkbox" class="form-control" id="CodigoGrupo"  readonly>
+          <input type="checkbox" class="form-control" id="CodigoGrupo"  readonly>
+        </div> </div>
+
+        <div class="col-md-4"> <div class="input-group">
+          <span class="input-group-text"> Moneda  </span>
+          <input type="checkbox" class="form-control" id="CodigoGrupo"  readonly>
+          <span class="input-group-text"> Moneda  </span>
+          <input type="checkbox" class="form-control" id="CodigoGrupo"  readonly>
+        </div> </div>
+
+        <input type="checkbox" class="form-control" id="CodigoGrupo"  readonly>
+      </div>
+
+      <div class="row crdbody">
+        <div class="col-md-3"> <div class="input-group">
+          <span class="input-group-text"> Contraseña </span>
+          <input type="password" class="form-control" id="CodigoGrupo"  readonly>
+        </div> </div>
+        <div class="col-md-3"> <div class="input-group">
+          <span class="input-group-text"> Factura </span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div> </div>
+      </div>
+
+    </div> </div> <!-- Ingreso de Caja FINAL -->
+
+    <div class="card crdbody">
+    <div class="card-header text-info"> <h4>Variable de Control</h4>  </div>
+    <div class="card-body">
+      
+      <div class="row crdbody">
+        <div class="col-1"> 
+          <span class="input-group-text"> Línea </span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div>  
+
+        <div class="col-2">  
+          <span class="input-group-text"> Cantidad </span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div>  
+
+        <div class="col-1">  
+          <span class="input-group-text"> Unidad </span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div>  
+
+        <div class="col-3">  
+          <span class="input-group-text"> Descripión </span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div>
+
+        <div class="col-2"> 
+          <span class="input-group-text"> Precio Unitario </span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div>  
+
+        <div class="col-1"> 
+          <span class="input-group-text"> IVA </span>
+          <input type="text" class="form-control" id="CodigoGrupo"  readonly>
+        </div> 
+
+        <div class="col-2">  
+          <span class="input-group-text"> ENTREGA </span>
+          <input type="date" class="form-control" id="CodigoGrupo"  readonly>
+        </div>  
+      </div>
+
+      <div class="row crdbody">
+        <table>
+          <thead>
+            <th> 1 </th>
+            <th>  </th>
+            <th>  </th>
+            <th>  </th>
+            <th>  </th>
+          </thead>
+          <tbody>
+            <th> 1ah </th>
+            <th>  </th>
+            <th>  </th>
+            <th>  </th>
+            <th>  </th>
+          </tbody>
+        </table>
+      </div>
+    </div> </div> <!-- Ingreso de Caja FINAL -->
   </div>
 </body>
 
